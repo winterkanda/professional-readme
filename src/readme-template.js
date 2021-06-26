1c35b4db27a106ca0d3759b6170f6c062a5033f2
Kiran Kanda <kkanda@dons.usfca.edu>
2021-06-25 20:49:43 -0700

    updated readme template

--- src/readme-template.js
+++ src/readme-template.js
@@ -28,4 +28,81 @@ const generateUsage = usageText => {
     return `
     ${usageText}
     `;
-}
+};
+
+const generateContributing = contributingText => {
+    if (!contributingText) {
+        return '';
+    }
+
+    return `
+    ${contributingText}
+    `;
+};
+
+const generateTesting = testingText => {
+    if (!testingText) {
+        return '';
+    }
+
+    return `
+    ${testingText}
+    `;
+};
+
+module.exports = templateData => {
+    console.log(templateData);
+
+    const { title, description, installation, usage, licenses, contributing, testing, email, gitHub, name } = templateData;
+
+    return `
+
+    # ${title}
+    ![badge](https://img.shields.io/badge/license-${licenses}-green)
+
+
+    ## Table of Contents  
+    [Description](#description)  
+    [Installation](#installation)  
+    [Usage](#usage)  
+    [Licenses](#licenses)  
+    [Testing](#testing)  
+    [Questions](#questions)  
+    
+    <a name="description"/>
+
+    ## Description
+    ${generateDescription(description)}
+    
+    <a name="installation"/>
+    
+    ## Installation Instructions
+    ${generateInstallation(installation)}
+    
+    <a name="usage"/>
+    
+    ## Usage Instructions
+    ${generateUsage(usage)}
+    
+    <a name="licenses"/>
+    
+    ## Licenses Used
+    ${licenses}.
+    <a name="contributions"/>
+
+    ## Contribution Guidelines
+    ${generateContributing(contributing)}
+    <a name="testing"/>
+    
+    ## Testing Instructions
+    ${generateTesting(testing)}
+    <a name="questions"/>
+    
+    ## Questions
+    Contact me with any additional questions either via my GitHub or email: 
+    * GITHUB: https://www.github.com/${gitHub}
+    * EMAIL: <mailto:${email}>
+    ### @${new Date().getFullYear()} ${name}
+    
+    `;  
+};
